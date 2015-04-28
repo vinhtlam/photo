177a881c838fe6b00e9f5e93549fdd424e2bf1c7
Vinh Lam <vinhtlam@yahoo.com>
2015-04-28 21:58:15 +0700

    done pagination

--- photo.sql
+++ photo.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.43, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: photo_gallery
 -- ------------------------------------------------------
--- Server version	5.5.41-0ubuntu0.14.04.1
+-- Server version	5.5.43-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -30,7 +30,7 @@ CREATE TABLE `comments` (
   `body` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `photograph_id` (`photograph_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `comments` (
 
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` VALUES (1,1,'2015-04-23 19:40:23','Vinh lam','I like this photo.'),(3,1,'2015-04-23 21:15:00','Tom','Woa, that\'s a great photo.'),(4,1,'2015-04-23 21:15:48','Tim','It isn\'t that good.'),(5,1,'2015-04-23 21:16:07','Vivian','It\'s ok a.');
+INSERT INTO `comments` VALUES (1,1,'2015-04-23 19:40:23','Vinh lam','I like this photo.'),(3,1,'2015-04-23 21:15:00','Tom','Woa, that\'s a great photo.'),(4,1,'2015-04-23 21:15:48','Tim','It isn\'t that good.'),(5,1,'2015-04-23 21:16:07','Vivian','It\'s ok a.'),(9,1,'2015-04-24 10:43:56','a;ldskhfj','a;ldsj02 230p23p09u'),(10,1,'2015-04-24 10:44:03','erglkj 23','alewk 089ut aedkjgf');
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,7 +57,7 @@ CREATE TABLE `photographs` (
   `size` int(11) NOT NULL,
   `caption` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +66,7 @@ CREATE TABLE `photographs` (
 
 LOCK TABLES `photographs` WRITE;
 /*!40000 ALTER TABLE `photographs` DISABLE KEYS */;
-INSERT INTO `photographs` VALUES (1,'haiquyen.jpg','image/jpeg',124393,'on the beach'),(2,'94f4467b5ab7b8.jpg','image/jpeg',169435,'Kate Upton Cover'),(3,'h3.jpgs.jpg','image/jpeg',31968,'chinese model1'),(5,'94f4467c8851a8.jpg','image/jpeg',131780,'Kate Sport Illustrast');
+INSERT INTO `photographs` VALUES (1,'haiquyen.jpg','image/jpeg',124393,'on the beach'),(2,'94f4467b5ab7b8.jpg','image/jpeg',169435,'Kate Upton Cover'),(3,'h3.jpgs.jpg','image/jpeg',31968,'chinese model1'),(5,'94f4467c8851a8.jpg','image/jpeg',131780,'Kate Sport Illustrast'),(6,'51c99782_5d30eb8f_screenhunter_17-jun.jpg','image/jpeg',50349,'Bikini vn girl 1'),(7,'369.jpg','image/jpeg',136466,'Car and ..'),(8,'IMG_0400.jpg','image/jpeg',455633,'Bikini vn girl 2'),(9,'tumblr_njp9e3mWaU1qij4o1o1_500.jpg','image/jpeg',87130,'kute sce');
 /*!40000 ALTER TABLE `photographs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,4 +106,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-04-23 21:31:03
+-- Dump completed on 2015-04-28 21:57:36
--- photogallery/public/admin/list_photos.php
+++ photogallery/public/admin/list_photos.php
@@ -5,7 +5,31 @@ require_once('../../includes/functions.php');
 if(!$session->is_logged_in()) {	redirect_to("login.php"); }
 // if(isset($session->message)){
 
-$photos = Photograph::find_all();
+//1. the current page number ($number_page)
+$page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+
+//2. records per page ($per_page)
+$per_page = 2;
+
+//3. total record count ($total_count)
+$total_count = Photograph::count_all();
+
+$pagination = new Pagination($page, $per_page, $total_count);
+
+// Find all photos
+//use pagination instead
+//$photos = Photograph::find_all();
+
+$sql = "SELECT * FROM photographs";
+$sql .= " LIMIT {$per_page}" ;
+$sql .= " OFFSET {$pagination->offset()}";
+
+$photos = Photograph::find_by_sql($sql);
+
+//need to add ?page=$page to all links we want to
+//maintain the current page (or store $page in $session)
+
+
 ?>
 
 <?php include_layout_template('admin_header.php'); ?>
@@ -43,10 +67,42 @@ $photos = Photograph::find_all();
 <?php endforeach; ?>
 </table>
 <br />
+
+<div id="pagination" style="clear: both;">
+<?php 
+
+	if ($pagination->total_pages() > 1) {
+
+		if($pagination->has_previous_page()) {
+			echo " <a href=\"list_photos.php?page="; 
+			echo $pagination->previous_page();
+			echo "\">&laquo; Previous</a> ";
+		}
+
+		for ($i =1; $i <= $pagination->total_pages(); $i++) {
+			if($i == $page) {
+				echo " <span class=\"seleted\">{$i}</span> ";
+			} else {
+				echo " <a href=\"list_photos.php?page={$i}\">{$i}</a> "; 
+			}	
+		} 
+
+		if($pagination->has_next_page()) {
+			echo " <a href=\"list_photos.php?page="; 
+			echo $pagination->next_page();
+			echo "\">&raquo; Next</a> ";
+		}
+	}
+
+?>
+</div>
+
+
+<br />
 <a href="photo_upload.php">Upload a new photograph</a>
 <br /> <br />
 
 <a href="logout.php">Logout</a>
-	</div>
+
 
 <?php include_layout_template('admin_footer.php'); ?>
