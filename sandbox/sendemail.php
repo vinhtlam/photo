e9d62230fed9f62139d2690ed939f37f43e1fd19
Vinh Lam <vinhtlam@yahoo.com>
2015-05-01 11:49:04 +0700

    add phpmailer

--- sandbox/sendemail.php
+++ sandbox/sendemail.php
@@ -1,11 +1,10 @@
 <?php
+ini_set('display_errors',1);
 
-$to = "neotv50@gmail.com";
+$to = "vinhtlam@yahoo.com";
 
 //Windows may not handle this format wee
 //$to = "Vin Lam <neotv50@gmail.com>" ;
-
-
 
 //multible recipients
 //$to = "neotv50@gmail.com, neotv50@yahoo.com";
@@ -16,13 +15,29 @@ $message = "This is a test mail.";
 //Ooptional: Wrap liens for old email programs
 //wrap at 70/72/75,78
 
-$message = wordwrap($message, 70);
+$message = wordwrap($message, 70, "\r\n");
 
-$from= "vinh@tuoitre.com.vn";
-$headers = "From: {$from}";
+$from= "vinhtlam@yahoo.com";
+$headers = "From: {$from}\n";
+$headers .= "Reply-To: {$from}\n";
+// $headers .= "Cc: {$to}\n";
+// $headers .= "Bcc: {$to}\n";
+$headers .= "X-Mailer: PHP/".phpversion()."\n";
+$headers .= "MIME-Version: 1.0\n";
+$headers .= "Content-Type: text/plain; charset=iso-8859-1";
 
 $result = mail($to, $subject, $message, $headers);
 
-echo $result ? 'sent' : 'error';
+$mail=mail($to, "Subject: $subject",$message );
+if($mail){
+  echo "Thank you for using our mail form";
+}else{
+  echo "Mail sending failed."; 
+}
+
+echo $result;
+echo $result ? 'Sent' : 'Error';
+
+echo phpinfo();
 
 ?>
