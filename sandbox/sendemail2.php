f5a83fd5676fca0183292da48c314e37fd94c331
vinhtlam <vinhtlam@yahoo.com>
2015-05-02 13:59:21 +0700

    sua lai PHPmailer1

--- sandbox/sendemail2.php
+++ sandbox/sendemail2.php
@@ -11,25 +11,40 @@ $message = "This is a test mail.";
 $message = wordwrap($message, 70, "\r\n");
 
 $from_name= "Vinh Neo";
-$from= "neotv50@gmail.com";
+// $from= "neotv50@gmail.com";
 
+$from = "vinhlam@yarsmail.com";
 //PHP mail version (default)
 
 $mail = new PHPMailer();
 
+// $mail->isSMTP();
+// $mail->SMTPSecure = 'tls';
+// $mail->Host = 'smtp.gmail.com';
+// $mail->Port = 587;
+// // $mail->Host = 'tls://smtp.gmail.com:587';
+
+// $mail->SMTPAuth = true;
+// $mail->Username = "neotv50@gmail.com";
+// $mail->Password = "7894568072";
+
 $mail->isSMTP();
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465;
-$mail->SMTPAuth = false;
-$mail->Username = "neotv50@gmail.com";
-$mail->Password = "7894568072";
+$mail->SMTPSecure = 'tls';
+$mail->Host = 'mail.yarsmail.com';
+$mail->Port = 587;
+// $mail->Host = 'tls://smtp.gmail.com:587';
+
+$mail->SMTPAuth = true;
+$mail->Username = "vinhlam@yarsmail.com";
+$mail->Password = "789456Lam";
+
 
 $mail->FromName = $from_name;
 $mail->From = $from;
 $mail->AddAddress($to, $to_name);
 $mail->Subject = $subject;
 $mail->Body = $message;
-
+$mail->SMTPDebug = 4;
 
 $result = $mail->Send();
 
